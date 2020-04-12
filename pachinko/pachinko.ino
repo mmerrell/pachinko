0b5f9c91b743ded67b6bd59ff931f02271897727
mmerrell <mmerrell@gmail.com>
2020-04-12 16:55:56 -0500

    Adding new path

--- pachinko/pachinko.ino
+++ pachinko/pachinko.ino
@@ -5,9 +5,9 @@
 #endif
 
 #define NEOPIXEL_PIN 6
-#define WAIT 2
+#define WAIT 5
 #define MAX_BALLS 8
-#define MAX_SEGMENTS 5
+#define MAX_SEGMENTS 6
 #define TOTAL_PATHS 5
 #define TIME_BETWEEN_LAUNCH 20  //This is the number of loops, not the actual time
 
@@ -22,15 +22,17 @@ int segments[][2] = {
   {  77,   -5 }, //7
   { 168,   33 }, //8
   { 168,    6 }, //9
-  { 153,  -34 }  //10
+  { 153,  -34 }, //10
+  { 200,   20 }  //11
 };
 
 int paths[][MAX_SEGMENTS] = {
-  { 0,             -1 },
-  { 1, 2, 3, 4,    -1 },
-  { 1, 5,          -1 },
-  { 6, 7, 8, 4,    -1 },
-  { 6, 7, 9, 10,   -1 }
+  { 0,              -1 },
+  { 1, 2, 3, 4,     -1 },
+  { 1, 5,           -1 },
+  { 6, 7, 8, 4,     -1 },
+  { 6, 7, 9, 10,    -1 },
+  { 6, 7, 8, 11, 4, -1 }
 };
 
 const unsigned char START_BUTTON_PIN = 8;         //Not PWM
@@ -81,12 +83,11 @@ void updateIoPins() {
 
 void setup() {
 //  Serial.begin(9600);
-  Serial.println("Ready...");
+//  Serial.println("Ready...");
 
   initIoPins();
   initArrays();
   initLights();
-  ballLaunch(4, getRandomBallColor());
 }
 
 uint32_t getRandomBallColor() {
@@ -111,7 +112,8 @@ void loop() {
     } else {
       if (startButtonLow) {
         startButtonLow = false;
-        ballLaunch(getRandomBallColor());
+        ballLaunch();
+//        ballLaunch(5, strip.Color(120, 120, 120));
         ignoreStartButtonCounter = 0;
       }
     }
@@ -124,14 +126,9 @@ void loop() {
 }
 
 void ballLaunch(int path, uint32_t color) {
-  Serial.println("Launching!!");
   for (int i=0; i<MAX_BALLS; i++) {
     if (ballPositions[i] == -1) {
-      Serial.print("Launching ball in position: ");
-      Serial.println(i);
       ballPaths[i] = path;
-      Serial.print("Using path: ");
-      Serial.println(ballPaths[i]);
       ballPositions[i] = 0;
       ballColors[i] = color;
       return;
@@ -140,20 +137,17 @@ void ballLaunch(int path, uint32_t color) {
 }
 
 void ballLaunch(uint32_t color) {
-  ballLaunch(random(0, 5), color);
+  int numPaths = sizeof(paths) / sizeof(int[MAX_SEGMENTS]);
+  ballLaunch(random(0, numPaths), color);
 }
 
 void ballLaunch() {
-  ballLaunch(random(0, 5), getRandomBallColor());
+  int numPaths = sizeof(paths) / sizeof(int[MAX_SEGMENTS]);
+  ballLaunch(random(0, numPaths), getRandomBallColor());
 }
 
 //Given a path and a position within the path, calculate which pixel to light up
 int calcPathPixel(int ballPath, int ballPosition) {
-//  Serial.print("Attempting to locate ball within Path: ");
-//  Serial.print(ballPath);
-//  Serial.print(", Position: ");
-//  Serial.println(ballPosition);
-
   //which segment are we in?
   int minSegIndex = 0;
   int maxSegIndex = 0;
@@ -164,35 +158,13 @@ int calcPathPixel(int ballPath, int ballPosition) {
       break;
     }
     maxSegIndex += abs(segments[paths[ballPath][i]][1]);
-//    Serial.print("Path Segment Index: ");
-//    Serial.print(i);
-//    Serial.print(", Actual Segment Index: ");
-//    Serial.print(paths[ballPath][i]);
-//    Serial.print(", Segment Length: ");
-//    Serial.print(abs(segments[paths[ballPath][i]][1]));
-//    Serial.print(", ballPosition: ");
-//    Serial.print(ballPosition);
-//    Serial.print(", minSegIndex: ");
-//    Serial.print(minSegIndex);
-//    Serial.print(", maxSegIndex: ");
-//    Serial.println(maxSegIndex);
-    
     if (ballPosition >= minSegIndex && ballPosition <= maxSegIndex) {
-//      Serial.print("Ball is located within Path Segment: ");
-//      Serial.print(i);
-//      Serial.print(", Actual Segment: ");
-//      Serial.println(paths[ballPath][i]);
-//      Serial.print("Between ");
-//      Serial.print(minSegIndex);
-//      Serial.print(" and ");
-//      Serial.println(maxSegIndex);
       segment = i;
       break;
     }
     minSegIndex = maxSegIndex + 1;
   }
   if (segment == -1) {
-    Serial.println("Ball has reached terminal");
     return -1;
   }
 
@@ -203,20 +175,9 @@ int calcPathPixel(int ballPath, int ballPosition) {
   
   if (segLength >= 0) {
     pixel = segStartingPoint + offset;
-//    Serial.println("Positive direction");
   } else {
     pixel = segStartingPoint - offset;
-//    Serial.println("Negative direction");
   }
-//  Serial.print("First pixel of segment ");
-//  Serial.print(paths[ballPath][segment]);
-//  Serial.print(": ");
-//  Serial.print(segStartingPoint);
-//  Serial.print(", Offset: ");
-//  Serial.print(offset);
-//  Serial.print(", Pixel: ");
-//  Serial.println(pixel);
-
   return pixel;
 }
 
@@ -228,13 +189,6 @@ void updateBalls() {
       trailingBallPixels2[i] = trailingBallPixels1[i];
       trailingBallPixels1[i] = ballPixels[i];
 
-      Serial.print("Pixel: ");
-      Serial.print(ballPixels[i]);
-      Serial.print(", Trailing Pixel 1: ");
-      Serial.print(trailingBallPixels1[i]);
-      Serial.print(", Trailing Pixel 2: ");
-      Serial.println(trailingBallPixels2[i]);
-
       ballPositions[i]++;
       int ballPosition = ballPositions[i];
       int ballPath = ballPaths[i];
@@ -242,8 +196,6 @@ void updateBalls() {
       int pixel = calcPathPixel(ballPath, ballPosition);
       ballPixels[i] = pixel;
       if (pixel == -1) {
-        Serial.print("Terminating Ball: ");
-        Serial.println(i);
         ballPaths[i] = -1;
         ballPositions[i] = -1;
         ballColors[i] = strip.Color(0, 0, 0);
@@ -252,23 +204,9 @@ void updateBalls() {
         strip.setPixelColor(trailingBallPixels2[i], strip.Color(0, 0, 0));
         return;
       }
-      
-
-
-      Serial.print("Ball: ");
-      Serial.print(i);
-      Serial.print(", Path: ");
-      Serial.print(ballPaths[i]);
-      Serial.print(", Position: ");
-      Serial.println(ballPositions[i]);
-      Serial.print("Pixel: ");
-      Serial.println(ballPixels[i]);
-
       strip.setPixelColor(pixel, color);
       strip.setPixelColor(trailingBallPixels1[i], strip.Color(10, 10, 10));
       strip.setPixelColor(trailingBallPixels2[i], strip.Color(0, 0, 0));
-
-
     }
   }
 
