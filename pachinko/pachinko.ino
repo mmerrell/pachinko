9880b90ae2ca65e72ad4d9b3ad0f6b8faea069ad
mmerrell <mmerrell@gmail.com>
2020-04-19 22:44:00 -0500

    Dynamic paths are working. Taking a snapshot while debug lines are still in place, and about to add new segments

--- pachinko/pachinko.ino
+++ pachinko/pachinko.ino
@@ -6,17 +6,15 @@
 
 #define NEOPIXEL_PIN        6
 #define WAIT                5
-#define MAX_BALLS           8
-#define MAX_SEGMENTS        6
-#define TOTAL_PATHS         5
+#define MAX_BALLS           5
 #define BRIGHTNESS          123
 #define TOTAL_PIXELS        360
-#define TIME_BETWEEN_LAUNCH 20  //The number of loops, not the actual time
+#define TIME_BETWEEN_LAUNCH 3  //The number of loops, not the actual time
 #define MAX_CONNECTIONS     4
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(TOTAL_PIXELS, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
 static int segments[][2] = {
-  {   0,  120 }, //0
+  {   0,  119 }, //0
   {   0,   73 }, //1
   { 167,  -14 }, //2
   { 174,   27 }, //3
@@ -26,7 +24,7 @@ static int segments[][2] = {
   {  77,   -5 }, //7
   { 168,   33 }, //8
   { 168,    6 }, //9
-  { 153,  -34 }, //10
+  { 153,  -33 }, //10
   { 200,   20 }, //11
   { 264,  -24 }, //12 
   { 265,   49 }, //13
@@ -61,21 +59,6 @@ static int connections[][MAX_CONNECTIONS] = {
 
 static int connectionCounts[sizeof(connections)/sizeof(int[MAX_CONNECTIONS])];
 
-static int paths[][MAX_SEGMENTS] = {
-  { 0,                 -1 }, //0
-  { 1,  2,  3,  4,     -1 }, //1
-  { 1,  5,             -1 }, //2
-  { 6,  7,  8,  4,     -1 }, //3
-  { 6,  7,  9, 10,     -1 }, //4
-  { 6,  7,  8, 11,  4, -1 }, //5
-  { 6,  7, 14, 12,     -1 }, //6
-  { 6,  7, 14, 13,     -1 }, //7
-  { 6,  7, 14, 16, 15, -1 }  //8
-};
-
-const int numPaths = sizeof(paths)/sizeof(int[MAX_SEGMENTS]);
-int pathLengths[numPaths];
-
 static uint32_t colorAry[] = {
   strip.Color(255, 255, 255),
   strip.Color(255, 0, 255),
@@ -86,6 +69,8 @@ static uint32_t colorAry[] = {
   strip.Color(255, 123, 0)
 };
 
+char data[100];
+
 //Ball Effects:
 // 0: Just the specified color
 // 1: Cycle through the palette, starting with the specifed color and going around the wheel
@@ -94,11 +79,11 @@ int ballEffects[MAX_BALLS];
 const unsigned char START_BUTTON_PIN = 8;         //Not PWM
 unsigned char startButtonState = HIGH;
 
-//keeps track of which path each ball is on
-int ballPaths[MAX_BALLS];
-
 //keeps track of which segment each ball is on
 int ballSegments[MAX_BALLS];
+
+//Which segments can be starting points
+int startingSegments[3] = { 0, 1, 6 };
 
 //keeps track of where the ball is in its path, as well as the trailing pixels
 int ballPositions[MAX_BALLS];
@@ -183,6 +168,7 @@ void loop() {
     } else {
       if (startButtonLow) {
         startButtonLow = false;
+        Serial.println("Launching Ball!!!");
         ballLaunch();
         ignoreStartButtonCounter = 0;
       }
@@ -197,7 +183,9 @@ void loop() {
 void ballLaunch(uint32_t color, int ballEffect) {
   for (int i=0; i<MAX_BALLS; i++) {
     if (ballPositions[i] == -1) {
-      ballSegments[i] = 0;
+      ballSegments[i] = startingSegments[random(0, 3)];
+      sprintf(data, "Choosing new segment for ball %d: %d", i, ballSegments[i]);
+      Serial.println(data);
       ballPositions[i] = 0;
       ballColors[i] = color;
       ballEffects[i] = ballEffect;
@@ -214,71 +202,60 @@ void ballLaunch() {
   ballLaunch(getRandomBallColor(), random(0, 2));
 }
 
-int getConnectionCount(int segIdx) {
-}
-
-//Given a path and a position within the path, calculate which pixel to light up
-int calcPathPixel(int ballIdx) {
-  int ballPosition = ballPositions[ballIdx];
-  int segment = ballSegments[ballIdx];
-  int segLength = segments[segment][1];
-  int segStartingPoint = segments[segment][0];
-
-  int pixel;
-  if (segLength >= 0) {
-    pixel = segStartingPoint + ballPosition;
-  } else {
-    pixel = segStartingPoint - ballPosition;
-  }
-
-  //We've determined which pixel to light, now determine whether 
-  //we're at the end of the segment, and if so, determine the new segment
-  if (ballPosition == abs(segLength)) {
-    ballSegments[ballIdx] = random(0, connectionCounts[segment]);
-  }
-  
-  return pixel;
-}
-
 void updateBalls() {
   for (int ballIdx=0; ballIdx<MAX_BALLS; ballIdx++) {
     //If the position is -1, the ball is inactive
     if (ballPositions[ballIdx] != -1) {
+//      sprintf(data, "Checkpoint 0: ball %d is active in segment %d, position %d, pixel %d", ballIdx, ballSegments[ballIdx], ballPositions[ballIdx], ballPixels[ballIdx]);
+//      Serial.println(data);
+      
       //before we increment the position in the path, set the trailing pixels
       trailingBallPixels2[ballIdx] = trailingBallPixels1[ballIdx];
       trailingBallPixels1[ballIdx] = ballPixels[ballIdx];
 
       //increment the ball's position in the path
       ballPositions[ballIdx]++;
+      
       int ballEffect = ballEffects[ballIdx];
-      int pixel = calcPathPixel(ballIdx);
-      ballPixels[ballIdx] = pixel; //this has to be done after incrementing and calculating the pixel, so the trailing pixels can be set correctly
 
-      //If pixel has been calculated as '-1', the ball has been terminated
-      if (pixel == -1) {
-        ballPaths[ballIdx] = -1;
-        ballPositions[ballIdx] = -1;
-        ballColors[ballIdx] = strip.Color(0, 0, 0);
-        strip.setPixelColor(pixel, ballColors[ballIdx]);
-        strip.setPixelColor(trailingBallPixels1[ballIdx], strip.Color(0, 0, 0));
-        strip.setPixelColor(trailingBallPixels2[ballIdx], strip.Color(0, 0, 0));
-        return;
+      if (segments[ballSegments[ballIdx]][1] >= 0) {
+        ballPixels[ballIdx] = segments[ballSegments[ballIdx]][0] + ballPositions[ballIdx];
+//        sprintf(data, "Incrementing: Ball %d to position %d: %d", ballIdx, ballPositions[ballIdx], ballPixels[ballIdx]);
+//        Serial.println(data);
+      } else {
+        ballPixels[ballIdx] = segments[ballSegments[ballIdx]][0] - ballPositions[ballIdx];
+//        sprintf(data, "Decrementing: Ball %d to position %d: %d", ballIdx, ballPositions[ballIdx], ballPixels[ballIdx]);
+//        Serial.println(data);
       }
-      
-      uint32_t color;
-      switch(ballEffect) {
-        case 1:
-          uint32_t mappedColor = map(ballPositions[ballIdx], 0, pathLengths[ballPaths[ballIdx]], ballColors[ballIdx], ballColors[ballIdx] + 65535);
-          int hue = 255;
-          color = strip.ColorHSV(mappedColor, hue, BRIGHTNESS);
-          break;
-        case 0:
-        default:
-          color = ballColors[ballIdx];
-      };
-      
-      strip.setPixelColor(pixel, color);
-      strip.setPixelColor(trailingBallPixels1[ballIdx], strip.ColorHSV(color, 123, 10));
+
+      //We've determined which pixel to light, now determine whether 
+      //we're at the end of the segment, and if so, determine the new segment
+//      sprintf(data, "Ball %d, Segment %d, Starting pixel %d, Length %d, Position %d", ballIdx, ballSegments[ballIdx], segments[ballSegments[ballIdx]][0], segments[ballSegments[ballIdx]][1], ballPositions[ballIdx]);
+//      Serial.println(data);
+      if (ballPositions[ballIdx] == abs(segments[ballSegments[ballIdx]][1])) {
+        sprintf(data, "Current Segment %d, Position %d", ballSegments[ballIdx], ballPositions[ballIdx]);
+        Serial.println(data);
+        if (connectionCounts[ballSegments[ballIdx]] == 0) {
+          ballPositions[ballIdx] = -1;
+          ballSegments[ballIdx] = -1;
+          ballColors[ballIdx] = strip.Color(0, 0, 0);
+          strip.setPixelColor(ballPixels[ballIdx], ballColors[ballIdx]);
+          strip.setPixelColor(trailingBallPixels1[ballIdx], strip.Color(0, 0, 0));
+          strip.setPixelColor(trailingBallPixels2[ballIdx], strip.Color(0, 0, 0));
+          strip.show();
+//          sprintf(data, "Ball %d terminated\n\tPixel: %d\n\tTrailing 1: %d\n\tTrailing 2: %d", ballIdx, ballPixels[ballIdx], trailingBallPixels1[ballIdx], trailingBallPixels2[ballIdx]);
+//          Serial.println(data);
+          return;
+        } else {
+          ballPositions[ballIdx] = 0;
+          int rndSeg = random(0, connectionCounts[ballSegments[ballIdx]]);
+          ballSegments[ballIdx] = connections[ballSegments[ballIdx]][rndSeg];
+//          sprintf(data, "%d, %d, %d, %d", rndSeg, connectionCounts[ballSegments[ballIdx]], connections[ballSegments[ballIdx]][rndSeg], ballSegments[ballIdx]);
+//          Serial.println(data);
+        }
+      }
+      strip.setPixelColor(ballPixels[ballIdx], ballColors[ballIdx]);
+      strip.setPixelColor(trailingBallPixels1[ballIdx], strip.ColorHSV(ballColors[ballIdx], 50, 10));
       strip.setPixelColor(trailingBallPixels2[ballIdx], strip.Color(0, 0, 0));
     }
   }
@@ -286,12 +263,12 @@ void updateBalls() {
   strip.show();
 }
 
-void lightPath(int pathIdx) {
-  int index = 0;
-  while (paths[pathIdx][index] != -1) {
-    strip.setPixelColor(paths[pathIdx][index], strip.Color(10, 10, 10));
-    index++;
-  }
-  strip.show();
-  delay(60000);
-}
+//void lightPath(int pathIdx) {
+//  int index = 0;
+//  while (paths[pathIdx][index] != -1) {
+//    strip.setPixelColor(paths[pathIdx][index], strip.Color(10, 10, 10));
+//    index++;
+//  }
+//  strip.show();
+//  delay(60000);
+//}
